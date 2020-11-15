40192dee7e1c57a5dc5555084411eab5e54734ff
Giang Phạm <56053037+vangiangcbcm@users.noreply.github.com>
2020-11-15 12:52:06 +0700

    Giang

--- README.md
+++ README.md
@@ -1,4 +1,2 @@
-﻿#  http://vangiangcbcm.github.io    
-Repo By Giang Phạm
-Kho lưu trữ tinh chỉnh cá nhân
-Kho là nơi tổng hợp ở mọi nguồn có cả sản phẩm của mình làm ra 😚
+# Nguồn thuộc về tác giả:
+# BIỆN VĂN NHẬT
--- description.js
+++ description.js
@@ -5,6 +5,13 @@ function iOSVersion() {
 	}
 	return false;
 }
+$(function() {
+  $("li").on("click",function() {
+	  if(this.id=="dnt") {
+		  $("#dnt_txt").html("You can donate USD via PayPal mail: julioverne"+"@"+"icloud.com");
+	  }
+  });
+});
 function loadPackageInfo() {
 	if (navigator.userAgent.search(/Cydia/) == -1) {
 		$("#showAddRepo_").show();
@@ -42,8 +49,8 @@ function loadPackageInfo() {
 				$("#compatitle_").show();
 				var ios_ver = iOSVersion();
 				if(ios_ver) {
-					$("#your_ios_").show();
-					$("#your_ios").html("iOS hiện tại : "+ios_ver);
+					$("#your_ios").show();
+					$("#your_ios").html("Current iOS: "+ios_ver);
 				}
 			}
 			if(decodeResp.changelog) {
@@ -54,9 +61,38 @@ function loadPackageInfo() {
 				$("#screenshot").html(decodeResp.screenshot);
 				$("#screenshot_").show();
 			}
+			if(decodeResp.open == true) {
+				$("#is_open_source_").show();
+			}
+			
         },
 		error: function (err) {
-			$("#errorInfo").html("Description unavailable for "+urlSelfParts[1]);
+			$("#errorInfo").html("Mô tả không có sẵn cho "+urlSelfParts[1]);
+		}
+	});
+}
+function loadRecentUpdates() {
+	var form_url = window.location.protocol+"//"+window.location.hostname+"/last.updates";
+	$.ajax({
+		url: form_url,
+		type: "GET",
+		cache: false,
+		crossDomain: true,
+		success: function (returnhtml) {
+			var decodeResp = eval('('+returnhtml+')');
+			var htmlnews = "";
+			for (var dicNow in decodeResp) {
+				var urlOpen = "cydia://package/"+decodeResp[dicNow].package;
+				if (navigator.userAgent.search(/Cydia/) == -1) {
+					urlOpen = window.location.protocol+"//"+window.location.hostname+"/description.html?id="+decodeResp[dicNow].package;
+				}
+				htmlnews +=  "<li><a href='"+urlOpen+"' target='_blank'><img class='icon' src='tweak.png'/><label>"+decodeResp[dicNow].name+" v"+decodeResp[dicNow].version+"</label></a></li>";
+			}
+			$("#updates").html(htmlnews);
+			$("#updates_").show();			
+        },
+		error: function (err) {
+			$("#updates_").hide();	
 		}
 	});
 }
